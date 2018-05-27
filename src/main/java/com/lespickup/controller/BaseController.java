dcc5e413030430a53a7e8887972479b7de4daa44
Vanessa Anderson <vanessa.anderson@timeinc.com>
2018-05-27 13:46:41 -0700

    no message

--- src/main/java/com/lespickup/controller/BaseController.java
+++ src/main/java/com/lespickup/controller/BaseController.java
@@ -1,15 +1,18 @@
 package com.lespickup.controller;
 
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.*;
 
 @RestController
-@RequestMapping("/api/persons")
+@RequestMapping("/api")
 public class BaseController {
 
-    @GetMapping
+    @GetMapping("persons")
     public ResponseEntity <LPResponse> hello() {
         LPResponse lpr = new LPResponse("Hello Vanessa!");
         return ResponseEntity.ok(lpr);
