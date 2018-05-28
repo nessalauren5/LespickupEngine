4770f0ae624fdf2ea41cfbf954dc590646607d12
Vanessa Anderson <vanessa.anderson@timeinc.com>
2018-05-27 20:09:47 -0700

    no message

--- src/main/java/com/lespickup/controller/BaseController.java
+++ src/main/java/com/lespickup/controller/BaseController.java
@@ -4,23 +4,28 @@ package com.lespickup.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.apache.logging.log4j.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import com.google.appengine.spi.ServiceFactoryFactory;
 
 @RestController
 @RequestMapping("/api")
-
 public class BaseController {
 
+    private static final Logger logger = Logger.getLogger(BaseController.class.getName());
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping("/")
     public ResponseEntity<String> helloagain() {
+        logger.log(Level.INFO,"inside helloagain");
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/persons")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<LPResponse> hello() {
+        logger.log(Level.INFO,"inside hello");
         LPResponse lpr = new LPResponse("Hello Vanessa!");
         return ResponseEntity.ok().body(lpr);
     }
