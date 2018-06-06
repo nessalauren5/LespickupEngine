b79d01545ffaff772a32c80c13dabfa27f4b26c9
Vanessa Anderson <vanessa.anderson@timeinc.com>
2018-06-06 00:52:03 -0700

    get llToken after user details

--- src/main/java/com/lespickup/controller/BaseController.java
+++ src/main/java/com/lespickup/controller/BaseController.java
@@ -2,6 +2,7 @@ package com.lespickup.controller;
 
 
 import com.lespickup.pojo.LPResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,13 @@ import java.util.logging.Logger;
 public class BaseController {
 
     private static final Logger logger = Logger.getLogger(BaseController.class.getName());
+
+    @Value("#{props['faappId']}")
+    public String FACEBOOK_APP_ID;
+
+    @Value("#{props['faappSec']}")
+    public String CLIENT_SECRET;
+
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping("/")
--- src/main/java/com/lespickup/controller/UserController.java
+++ src/main/java/com/lespickup/controller/UserController.java
@@ -4,8 +4,6 @@ import com.lespickup.pojo.LPResponse;
 import com.lespickup.util.ParameterStringBuilder;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.facebook.api.Facebook;
 import org.springframework.web.bind.annotation.*;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -15,14 +13,14 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/user")
 public class UserController extends BaseController {
 
-    private Facebook facebook;
-    private ConnectionRepository connectionRepository;
-
+    private static final Logger logger = Logger.getLogger(UserController.class.getName());
     @PostMapping("/signin/{token}")
     public ResponseEntity<LPResponse> signin(@PathVariable("token") String aToken) {
         LPResponse lpr;
@@ -36,14 +34,30 @@ public class UserController extends BaseController {
             con.setReadTimeout(5000);
             HashMap<String, String> parameters = new HashMap<>();
             parameters.put("access_token", aToken);
+            parameters.put("fields","id,name,email");
             con.setDoOutput(true);
             DataOutputStream out = new DataOutputStream(con.getOutputStream());
             out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
             out.flush();
             out.close();
-            JSONObject response = getFacebookUser(con);
-            BigInteger id = response.getBigInteger("id");
-            lpr = new LPResponse("id=" + id.toString());
+            JSONObject response = getFacebookResult(con);
+            logger.log(Level.INFO,response.toString(5));
+            url = new URL("https://graph.facebook.com/oauth/access_token");
+            con.disconnect();
+            con = (HttpsURLConnection) url.openConnection();
+            parameters.remove("access_token");
+            parameters.remove("fields");
+            parameters.put("grant_type","fb_exchange_token");
+            parameters.put("client_id",FACEBOOK_APP_ID);
+            parameters.put("client_secret",CLIENT_SECRET);
+            parameters.put("fb_exchange_token",aToken);
+            con.setDoOutput(true);
+            DataOutputStream out2 = new DataOutputStream(con.getOutputStream());
+            out2.writeBytes(ParameterStringBuilder.getParamsString(parameters));
+            out2.flush();
+            out2.close();
+            JSONObject llResponse = getFacebookResult(con);
+            lpr = new LPResponse(llResponse.toString(5));
 
         }
         catch (MalformedURLException e) {
@@ -56,7 +70,7 @@ public class UserController extends BaseController {
         return ResponseEntity.ok().body(lpr);
     }
 
-    public JSONObject getFacebookUser(HttpsURLConnection connection) throws IOException{
+    public JSONObject getFacebookResult(HttpsURLConnection connection) throws IOException{
         try (BufferedReader rd = new BufferedReader(new InputStreamReader(
                 connection.getInputStream()))) {
             return new JSONObject(readAll(rd));
